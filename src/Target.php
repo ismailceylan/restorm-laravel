aa419084ae329597ec3d38872f63edb8e28a869e
Ismail Ceylan <ismaillceylan@gmail.com>
2024-05-19 17:21:33 +0300

    fix(target): make sure index numbers are used as fields

--- src/Target.php
+++ src/Target.php
@@ -5,14 +5,22 @@ namespace Iceylan\Restorm;
 class Target
 {
 	public array $models;
-	public string $field;
+	public string|null $field;
 
 	public function __construct( string $target )
 	{
 		$parts = explode( '.', $target );
 
-		$this->field = array_pop( $parts );
-		$this->models = $parts;
+		if( count( $parts ) === 1 && ! is_numeric( $parts[ 0 ]))
+		{
+			$this->models[] = $parts[ 0 ];
+			$this->field = null;
+		}
+		else
+		{
+			$this->field = array_pop( $parts );
+			$this->models = $parts;
+		}
 	}
 
 	public function isRelation(): bool
